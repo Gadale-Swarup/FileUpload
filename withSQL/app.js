fd8faf69e4eda7e4920bf534d3b4ca5e0ef3b009
Swarup-Gadale <swarup.gadale@gmail.com>
2024-07-02 12:42:47 +0530

    upload many files

--- withSQL/app.js
+++ withSQL/app.js
@@ -29,23 +29,41 @@ app.post('/upload',upload.single('file'),(req,res)=>{
     })
 })
 
- app.get('/download/:filename',(req,res)=>{
-    const sql = 'select * from files where filename=?';
 
-    db.query(sql,[req.params.filename],(err,results)=>{
-        if(err){
-            return res.status(500).send('Error in retrieving file');
+app.post('/uploadmul', upload.array('files', 12), (req, res) => {
+    const files = req.files;
+    console.log(req.files);
+    
+    if (!files || files.length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+    
+    const queryForInsert = 'INSERT INTO files (filename,path,originalname,mimetype,size) VALUES ?';
+    const values = files.map(file => [file.filename, file.path, file.originalname, file.mimetype, file.size]);
+    
+    db.query(queryForInsert, [values], (err) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send('Error uploading files');
         }
-        if(results.length === 0){
-            return res.status(404).send('File not found');
-        }
-        const file = results[0];
-        res.download(file.path,file.filename);
-     });
+        res.status(201).send('Files uploaded successfully');
+    });
+});
+
+app.get('/download/:filename',(req,res)=>{
+   const sql = 'select * from files where filename=?';
+
+   db.query(sql,[req.params.filename],(err,results)=>{
+       if(err){
+           return res.status(500).send('Error in retrieving file');
+       }
+       if(results.length === 0){
+           return res.status(404).send('File not found');
+       }
+       const file = results[0];
+       res.download(file.path,file.filename);
+    });
 })
-
-
-
 
 
 
